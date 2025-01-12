a6b1910795958a324e520f816da33b84579f9d8a
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-01-12 14:24:22 -0300

    feat: instalação do express-async-errors e criação de tratativa de erros global

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
+require("express-async-errors");
+
 const express = require("express");
 const routes = require("./routes");
+const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
 
-app.use(routes) // Utilizando as rotas criadas
+app.use(routes); // Utilizando as rotas criadas
+
+// Tratativa de Erros
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            type: "Error",
+            message: error.message
+        });
+    }
+
+    // Internal Server Error
+    console.error(error);
+
+    return response.status(500).json({
+        type: "Error",
+        message: "Internal Server Error"
+    });
+});
 
 const PORT = 3000;
 
