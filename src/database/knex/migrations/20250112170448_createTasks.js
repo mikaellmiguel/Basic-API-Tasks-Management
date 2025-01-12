b8de772139b58be782c10c7cc22d4a9f1c51060e
Mikaell Miguel da Silva <mikaell.miguel.br@gmail.com>
2025-01-12 14:08:38 -0300

    feat: migration para a criação de tabela das tarefas no bd

---
+++ src/database/knex/migrations/20250112170448_createTasks.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = knex => knex.schema.createTable("tasks", table => {
+    table.increments("id");
+    table.text("title");
+    table.text("description");
+    table.text("status");
+    table.date("due_date").nullable();
+})
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable("tasks");
